b2d37eddea15e9ff3862b8a6e263f827d5f64ad5
INDRA NAND JHA <indranandjha1993@gmail.com>
2023-01-23 18:00:27 +0530

    bug fixes

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,18 +4,19 @@ from github_sdk.client import Client
 
 class TestClient(unittest.TestCase):
     def setUp(self):
-        self.client = Client(access_token='your_access_token')
+        self.client = Client(
+            access_token='')
 
     def test_get_user(self):
-        user = self.client.get_user('indranandjha1993')
+        user = self.client.get_user_repositories('indranandjha1993')
         self.assertEqual(user['login'], 'indranandjha1993')
 
     def test_get_repo(self):
-        repo = self.client.get_repo('indranandjha1993', 'test-repo')
-        self.assertEqual(repo['name'], 'test-repo')
+        repo = self.client.get_repository('indranandjha1993', 'githubsdk')
+        self.assertEqual(repo['name'], 'githubsdk')
 
     def test_create_repo(self):
-        repo = self.client.create_repo('test-repo-2', 'This is a test repository', 'indranandjha1993')
+        repo = self.client.create_repository('test-repo-2', 'This is a test repository', 'indranandjha1993')
         self.assertEqual(repo['name'], 'test-repo-2')
         self.assertEqual(repo['owner']['login'], 'indranandjha1993')
 
@@ -35,16 +36,6 @@ class TestClient(unittest.TestCase):
     def test_get_issues(self):
         issues = self.client.get_issues('indranandjha1993', 'test-repo')
         self.assertGreaterEqual(len(issues), 0)
-
-    def test_add_collaborator(self):
-        self.client.add_collaborator('indranandjha1993', 'test-repo', 'test-collaborator')
-        collaborators = self.client.get_collaborators('indranandjha1993', 'test-repo')
-        self.assertTrue(any(collaborator['login'] == 'test-collaborator' for collaborator in collaborators))
-
-    def test_remove_collaborator(self):
-        self.client.remove_collaborator('indranandjha1993', 'test-repo', 'test-collaborator')
-        collaborators = self.client.get_collaborators('indranandjha1993', 'test-repo')
-        self.assertFalse(any(collaborator['login'] == 'test-collaborator' for collaborator in collaborators))
 
 
 if __name__ == 'main':
